94b73f3594b45abc6e09fa4255df38deea140235
Livardy Wufianto <LW130003@gmail.com>
2026-03-05 00:02:51 +0800

    feat: initial clean setup for PatchTST

--- PatchTST_self_supervised/datautils.py
+++ PatchTST_self_supervised/datautils.py
@@ -19,7 +19,8 @@ def get_dls(params):
     if not hasattr(params,'use_time_features'): params.use_time_features = False
 
     if params.dset == 'ettm1':
-        root_path = '/data/datasets/public/ETDataset/ETT-small/'
+        # root_path = '/data/datasets/public/ETDataset/ETT-small/'
+        root_path = "/Users/livardywufianto/Projects/PatchTST/dataset/ETT-small/"
         size = [params.context_points, 0, params.target_points]
         dls = DataLoaders(
                 datasetCls=Dataset_ETT_minute,
---
+++ notebooks/ct_patch_tst_layer.py
@@ -0,0 +1,93 @@
+
+class ChannelAttention(nn.Module):
+
+    def forward(self, src: Tensor, 
+                prev: Optional[Tensor]=None):
+
+        # src shape (M, B*N, P) or (n_vars, bs*num_patch, self.d_model)
+
+
+        
+        if self.pre_norm:
+            src = self.norm_attn(src)
+        ## Multi-Head attention
+        if self.res_attention:
+            src2, attn, scores = self.self_attn(src, src, src, prev)
+        else:
+            src2, attn = self.self_attn(src, src, src)
+        if self.store_attn:
+            self.attn = attn
+        
+        ## Add & Norm
+        src = src + self.dropout_attn(src2) # Add: residual connection with residual dropout
+        if not self.pre_norm:
+            src = self.norm_attn(src)        
+
+        # Feed forward channel independence
+        # shape (B*M, N, P)
+
+        # Feed-forward sublayer
+        if self.pre_norm:
+            src = self.norm_ffn(src)
+        ## Position-wise Feed-Forward
+        src2 = self.ff(src)
+        ## Add & Norm
+        src = src + self.dropout_ffn(src2) # Add: residual connection with residual dropout
+        if not self.pre_norm:
+            src = self.norm_ffn(src)
+
+        if self.res_attention:
+            return src, scores
+        else:
+            return src
+
+
+
+
+class CTTSTEncoderLayer(nn.Module):
+
+    def __init__(self, d_model, n_heads, d_ff=256, store_attn=False,
+                 norm='BatchNorm', attn_dropout=0, dropout=0., bias=True, 
+                activation="gelu", res_attention=False, pre_norm=False):
+        super().__init__()
+        assert not d_model%n_heads, f"d_model ({d_model}) must be divisible by n_heads ({n_heads})"
+        d_k = d_model // n_heads
+        d_v = d_model // n_heads
+
+        # Multi-Head attention
+        self.res_attention = res_attention
+        self.self_attn = MultiheadAttention(d_model, n_heads, d_k, d_v, attn_dropout=attn_dropout, proj_dropout=dropout, res_attention=res_attention)
+
+        # Add & Norm
+        self.dropout_attn = nn.Dropout(dropout)
+        if "batch" in norm.lower():
+            self.norm_attn = nn.Sequential(Transpose(1,2), nn.BatchNorm1d(d_model), Transpose(1,2))
+        else:
+            self.norm_attn = nn.LayerNorm(d_model)
+
+        # Position-wise Feed-Forward
+        self.ff = nn.Sequential(nn.Linear(d_model, d_ff, bias=bias),
+                                get_activation_fn(activation),
+                                nn.Dropout(dropout),
+                                nn.Linear(d_ff, d_model, bias=bias))
+
+        # Add & Norm
+        self.dropout_ffn = nn.Dropout(dropout)
+        if "batch" in norm.lower():
+            self.norm_ffn = nn.Sequential(Transpose(1,2), nn.BatchNorm1d(d_model), Transpose(1,2))
+        else:
+            self.norm_ffn = nn.LayerNorm(d_model)
+
+        self.pre_norm = pre_norm
+        self.store_attn = store_attn
+    
+    def forward(
+        self, src: Tensor, 
+        prev: Optional[Tensor]=None):
+
+        if self.pre_norm:
+            src = self.norm_attn(src)
+
+        if self.res_attention
+            
+        
